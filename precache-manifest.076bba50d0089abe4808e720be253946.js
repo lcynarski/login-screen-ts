0d3e4eff0400001588563572a8dc88d3953f3fb1
Lukasz Cynarski <l.cynarski@gmail.com>
2020-06-29 13:22:49 +0200

    Updates

---
+++ precache-manifest.076bba50d0089abe4808e720be253946.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "842613430ea238abe1da62f8d4a452c5",
+    "url": "/login-screen-ts/index.html"
+  },
+  {
+    "revision": "d79849068287cbe1bafe",
+    "url": "/login-screen-ts/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7a25acfca12ce6b1b4b9",
+    "url": "/login-screen-ts/static/js/2.36df62f3.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/login-screen-ts/static/js/2.36df62f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d79849068287cbe1bafe",
+    "url": "/login-screen-ts/static/js/main.1981a18a.chunk.js"
+  },
+  {
+    "revision": "207137a0188e096185be",
+    "url": "/login-screen-ts/static/js/runtime-main.f00b0921.js"
+  },
+  {
+    "revision": "adc51480c50b660e9a8dadc9503eb0ea",
+    "url": "/login-screen-ts/static/media/background-0.adc51480.jpg"
+  },
+  {
+    "revision": "82116b64cbf1d5542747b9c11dee274a",
+    "url": "/login-screen-ts/static/media/dogGif1.82116b64.gif"
+  }
+]);
